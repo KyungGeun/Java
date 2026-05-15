025883c37b08187e807ff28193fb0818799b21a7
OhKyungGeun <aokg20060228@gmail.com>
2026-05-15 11:36:19 +0900

    Java instanceof 개념  학습

--- main/src/OKGMain.java
+++ main/src/OKGMain.java
@@ -1,35 +1,23 @@
-class Point {
-   private int x, y;
-   public Point() {
-      this.x = this.y = 0;
-   }
-
-   public Point(int x, int y) {
-      this.x = x;
-      this.y = y;
-   }
-
-   public void showPoint() {
-      System.out.println(" (" + x + ", " + y + ")");
-   }
-}
-
-class ColorPoint extends Point {
-   private String color;
-   public ColorPoint(int x, int y, String color) {
-      super(x, y);
-      this.color = color;
-   }
-
-   public void showColorPoint() {
-      System.out.print(color);
-      showPoint();
-   }
-}
+class Person { }
+class Student extends Person { }
+class Researcher extends Person { }
+class Professor extends Researcher { }
 
 public class OKGMain {
-   public static void main(String[] var0) {
-      ColorPoint cp = new ColorPoint(5,7,"BlueViolet");
-      cp.showColorPoint();
+   static void print(Person p) {
+      if(p instanceof Person) { System.out.print("Person "); }
+      if(p instanceof Student) { System.out.print("Student "); }
+      if(p instanceof Researcher) { System.out.print("Researcher "); }
+      if(p instanceof Person) { System.out.print("Professor "); }
+      System.out.println();
+   }
+
+   public static void main(String[] args) {
+      System.out.print("new Student() ->\t");
+      System.out.println(new Student());
+      System.out.print("new Researcher() ->\t");
+      System.out.println(new Researcher());
+      System.out.print("new Professor() ->\t");
+      System.out.println(new Professor());
    }
 }
